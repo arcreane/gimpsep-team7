c43db00eef7a9398c09e804f2b13ff190fd3e950
DWP8436 <juliogarciabarrena@gmail.com>
2025-04-25 19:09:00 +0200

    Added resizing functionality and first main terminal interface

--- CVProject/CVProject/main.cpp
+++ CVProject/CVProject/main.cpp
@@ -3,16 +3,66 @@
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 
+#include "resize.cpp"
+
 int main()
 {
-	cv::Mat image = cv::imread("../img/lena.jpg");
-	if (image.empty())
-	{
-		std::cout << "Could not read the image: " << std::endl;
-		return 1;
+	while (true) {
+		std::string fileName;
+		int typeOfFile; //0 == image, 1 == video, 2==exit
+		int operation;
+		bool exitProgram = false;
+
+		std::cout << "Welcome to the Image Editor" << std::endl;
+
+		std::cout << "HighGUI backend: " << cv::getBuildInformation() << std::endl;
+
+	
+
+		std::cout << "Please type number of desired operation" << std::endl;
+		std::cout << "0: exit" << std::endl;
+		std::cout << "1: Dilation/Erosion" << std::endl;
+		std::cout << "2: Resizing" << std::endl;
+		std::cout << "3: Lighten/Darken" << std::endl;
+		std::cout << "4: Panorama/Stitching" << std::endl;
+		std::cout << "5: Canny edge detection" << std::endl;
+		std::cin >> operation;
+
+		switch (operation)
+		{
+		case 0:
+			exitProgram = true;
+			break;
+		case 1:
+			std::cout << "Chosen: Dilation/Erosion" << std::endl;
+			//Function for dilation
+			break;
+
+		case 2:
+			std::cout << "Chose: Resizing" << std::endl;
+			resizeImage(); 	//Function for Resizing
+
+			break;
+
+		case 3:
+			std::cout << "Chosen: Lighten/Darken" << std::endl;
+			break;
+
+		case 4:
+			std::cout << "Chosen: Panorama/Stitching" << std::endl;
+			break;
+
+		case 5:
+			std::cout << "CHosen: Canny edge detection" << std::endl;
+			break;
+		}
+
+		cv::destroyAllWindows();
+
+		if (exitProgram) {
+			break;
+		}
 	}
-	cv::namedWindow("Display window");
-	cv::imshow("Display window", image);
-	cv::waitKey(0);
+
 	return 0;
 }
---
+++ CVProject/CVProject/resize.cpp
@@ -0,0 +1,165 @@
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+
+#include <iostream>
+
+
+static int width;
+static int height;
+static int size;
+static bool resizingFinished = false;
+
+void static onConfirmButton(int state, void* userdata) {
+	resizingFinished = true;
+}
+
+void static resizeImage() {
+	while (true){
+	std::string fileName;
+	cv::Mat copyImage;
+	std::string input;
+	std::string textOfWindow1 = "Press [C] to confirm edit";
+	std::string textOfWindow2 = "press[P] to swap between proportional";
+	std::string textOfWindow3 = "resizing and separate resizing";
+	bool activateProportionalScaling = false;
+
+
+	resizingFinished = false;
+	int key = -1;
+	
+
+
+
+
+	std::cout << "Welcome to the Resizing Function" << std::endl;
+	std::cout << "Please enter file name" << std::endl;
+	std::cin >> fileName; //Personalised file name entry
+
+	//Making sure user can leave program when they want
+
+	cv::Mat image = cv::imread("../img/" + fileName);
+	if (image.empty())
+	{
+		std::cout << "Could not read the image: " << std::endl;
+		break;
+
+	}
+
+	//creating windows
+	cv::namedWindow("Original Image", cv::WINDOW_AUTOSIZE);
+	cv::namedWindow("New Image", cv::WINDOW_AUTOSIZE);
+	cv::namedWindow("Trackbar Width", cv::WINDOW_AUTOSIZE);
+	cv::namedWindow("Trackbar Height", cv::WINDOW_AUTOSIZE);
+
+	cv::namedWindow("TextWindow", cv::WINDOW_AUTOSIZE);
+
+	//Adding text
+	cv::Mat imageText = cv::Mat::zeros(400, 600, CV_8UC3);  // blank image for text
+	cv::putText(imageText,
+		textOfWindow1,					//Text
+		cv::Point(50, 70),				//Position
+		cv::FONT_HERSHEY_SIMPLEX,		//Font
+		0.7,							//Font Scale
+		cv::Scalar(255, 255, 255),		//Color (white)
+		2);								//thickness
+	cv::putText(imageText,
+		textOfWindow2,					//Text
+		cv::Point(50, 100),				//Position
+		cv::FONT_HERSHEY_SIMPLEX,		//Font
+		0.7,							//Font Scale
+		cv::Scalar(255, 255, 255),		//Color (white)
+		2);								//thickness
+
+	cv::putText(imageText,
+		textOfWindow3,					//Text
+		cv::Point(50, 130),				//Position
+		cv::FONT_HERSHEY_SIMPLEX,		//Font
+		0.7,							//Font Scale
+		cv::Scalar(255, 255, 255),		//Color (white)
+		2);								//thickness
+
+	width = image.cols;
+	height = image.rows;
+	size = 100;
+
+	cv::createTrackbar("Width", "Trackbar Width", &width, 4000);
+	cv::createTrackbar("Height", "Trackbar Height", &height, 4000);
+
+
+
+	//Button doesn't work
+	//cv::createButton("Confirm Size", onConfirmButton, 0, false);
+	copyImage = image;
+	cv::imshow("Original Image", image);
+	cv::imshow("TextWindow", imageText);
+
+	while (!resizingFinished && !image.empty()) {
+		if (!activateProportionalScaling) {
+
+			int safeWidth = std::max(1, width); //Avoids crash with resize of 0
+			int safeHeight = std::max(1, height); //Avoids crash with resize of 0
+
+			cv::resize(image, copyImage, cv::Size(safeWidth, safeHeight), 0, 0);
+		} else {
+
+			double safeSize = std::max(1, size)/100.0;
+			cv::resize(image, copyImage, cv::Size(), safeSize, safeSize);
+
+		}
+		cv::imshow("New Image", copyImage);
+		key = cv::waitKey(30);
+		std::cout << key << std::endl;
+
+		if (key == 'p') { //Press p to change the type of display for the type of resizing
+			activateProportionalScaling = !activateProportionalScaling;
+			if (activateProportionalScaling) {
+				//Case changing to proportional
+				cv::destroyWindow("Trackbar Width");
+				cv::destroyWindow("Trackbar Height");
+
+				cv::namedWindow("Trackbar Proportional", cv::WINDOW_AUTOSIZE);
+				cv::createTrackbar("Size", "Trackbar Proportional", &size, 400);
+
+			}
+			else {
+				//Case changing to non proportional resizing
+				cv::destroyWindow("Trackbar Proportional");
+
+				cv::namedWindow("Trackbar Width", cv::WINDOW_AUTOSIZE);
+				cv::namedWindow("Trackbar Height", cv::WINDOW_AUTOSIZE);
+
+				cv::createTrackbar("Width", "Trackbar Width", &width, 4000);
+				cv::createTrackbar("Height", "Trackbar Height", &height, 4000);
+
+			}
+			image = copyImage;
+
+		}
+
+		if (key == 'c') {
+			resizingFinished = true; //Press c to cfinish editing
+		}
+		std::cout << key << std::endl;
+	}
+
+	cv::destroyAllWindows();
+
+	std::cout << "Do you want to save the image? [Y/N]" << std::endl;
+	std::cin >> input;
+	if (input == "Y") {
+		std::cout << "Type the name of the new image (with extension)" << std::endl;
+		std::cin >> input;
+		cv::imwrite("../img/" + input, copyImage);
+	}
+
+	std::cout << "Do you want to go to main menu? [Y/N]" << std::endl;
+	std::cin >> input;
+
+		if (input == "Y") {
+			break;
+		}
+	}
+
+	
+}
